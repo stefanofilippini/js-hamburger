9b594b261c23044e321520f7b571854421a223f5
stefanofilippini <filippini.stefano96@gmail.com>
2021-10-22 16:26:55 +0200

    done exercise

---
+++ main.js
@@ -0,0 +1,16 @@
+const element = document.querySelector ('.fas.fa-bars')
+const element_1 = document.querySelector ('.hamburger-menu')
+const element_2 = document.querySelector ('.fas.fa-times')
+
+
+element.addEventListener ( 'click',
+    function() {
+        element_1.className = element_1.classList + " active"
+    }
+)
+
+element_2.addEventListener ( 'click',
+    function() {
+        element_1.className = "hamburger-menu"
+    }
+)
